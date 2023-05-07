9b83e631629105e6043d1e0534a826af022ad4fc
manoj5199 <manojadhi6@gmail.com>
2023-05-07 10:45:38 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
+
+const date = () => {
+  return new Date();
+};
+
+function Clock() {
+  const [time, setTime] = useState(date().toLocaleTimeString());
+  const refreshClock = () => setTime(date().toLocaleTimeString());
+
+  useEffect(() => {
+    const intID = setInterval(() => {
+      refreshClock();
+    }, 1000);
+
+    return () => {
+      clearInterval(intID);
+    };
+  }, []);
+
+  return <span>{time}</span>;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Clock />
       </header>
     </div>
   );
